28a852a2b8abb4d9ec45275a4dd6d972cf78176c
Eyeball Edward <edwardyeung.private@gmail.com>
2018-01-21 20:25:41 +0800

    make admiralty selectable

--- application/static/scripts/mrt-timings.js
+++ application/static/scripts/mrt-timings.js
@@ -1,6 +1,7 @@
 window.addEventListener('load', () => {
     $('#mrt-stations').on('change', () => {
         var currentStation = $(`#mrt-stations option:nth-child(${$('#mrt-stations').selectedIndex + 1})`).textContent;
+        if (currentStation === 'Select a station') return;
         location = '/mrt/timings/' + currentStation;
     });
 });
