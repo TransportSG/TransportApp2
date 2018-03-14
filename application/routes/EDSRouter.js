03d53b68ce5a88f0b4b84c35e75ebd67a098085a
Eyeball Edward <edwardyeung.private@gmail.com>
2018-03-14 21:23:28 +0800

    add in ter data

--- application/routes/EDSRouter.js
+++ application/routes/EDSRouter.js
@@ -1,11 +1,13 @@
 const Router = require('./Router');
 const BusServiceRepository = require('../../database/BusServiceRepository');
+const BusStopsRepository = require('../../database/BusStopsRepository');
 const DatabaseConnectionManager = require('../../database/DatabaseConnectionManager');
 
 class EDSRouter extends Router {
 
     static init() {
         EDSRouter.busServices = new BusServiceRepository(DatabaseConnectionManager.getConnection('TransportApp'));
+        EDSRouter.busStops = new BusStopsRepository(DatabaseConnectionManager.getConnection('TransportApp'));
     }
 
 
@@ -13,11 +15,26 @@ class EDSRouter extends Router {
         let svc = req.params.svc;
         if (!svc) {res.end(400); return;}
         EDSRouter.busServices.findOne(svc, (err, svc) => {
+            var promises = [];
+            var ints = [];
 
-            res.header('Access-Control-Allow-Origin', '*');
+            svc.interchanges.forEach(int => {
+                promises.push(new Promise((a, r) => {
+                    EDSRouter.busStops.findOne(int, (err, intStop) => {
+                        ints.push(intStop);
+                        a();
+                    });
+                }));
+            });
 
-            res.json(svc);
-            res.end();
+            Promise.all(promises).then(() => {
+                svc.interchanges = ints;
+
+                res.header('Access-Control-Allow-Origin', '*');
+
+                res.json(svc);
+                res.end();
+            });
         })
 
     }
