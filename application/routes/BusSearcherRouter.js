922c56bb8e8c5ddc22f547cbb4a2f6a711ae5ef3
Eyeball Edward <edwardyeung.private@gmail.com>
2018-03-28 22:11:07 +0800

    a buncha filters in

--- application/routes/BusSearcherRouter.js
+++ application/routes/BusSearcherRouter.js
@@ -23,17 +23,22 @@ class BusSearcherRouter extends Router {
     	res.render('bus-searcher/index.pug');
     }
 
-    static search(req, res) {
-        if (!req.body.query) {
-            res.status(400).json({
-                error: 'No query provided!'
+    static getSvcsFromInput(parsedData) {
+        let possibleSvcs = [];
+
+        possibleSvcs = possibleSvcs.concat(parsedData.services);
+
+        if (parsedData.depots)
+            parsedData.depots.forEach(depot => {
+                let svcsInDepot = BusDepotData[depot];
+                possibleSvcs = possibleSvcs.concat(svcsInDepot);
             });
-            return;
-        }
 
-        var timingsCache = BusTimings.getTimings();
+        return possibleSvcs;
+    }
 
-        var parsed = TextParser.parse(req.body.query, {
+    static parseData(query) {
+        return TextParser.parse(query, {
             services: {
                 type: /(!?\d+[GWABC#M]?)/,
                 canRepeat: true
@@ -46,9 +51,68 @@ class BusSearcherRouter extends Router {
             }
         });
 
-        var possibleTimings = {};
+    }
 
-        
+    static filterServices(timings, svcList) {
+        let possibleTimings = {};
+        let busStopCodes = Object.keys(timings);
+
+        busStopCodes.forEach(busStopCode => {
+            let busStopTimings = timings[busStopCode];
+
+            let filteredSvcs = busStopTimings.filter(busServiceTiming => {
+                return svcList.includes(busServiceTiming.service);
+            });
+
+            if (filteredSvcs.length > 0) {
+                possibleTimings[busStopCode] = filteredSvcs;
+            }
+        });
+
+        return possibleTimings;
+    }
+
+    static filterByNWAB(timings, wheelchair) {
+        let possibleTimings = {};
+        let busStopCodes = Object.keys(timings);
+
+        busStopCodes.forEach(busStopCode => {
+            let busStopTimings = timings[busStopCode];
+
+            busStopTimings.forEach(busServiceTiming => {
+                let matchingTimings = busServiceTiming.timings.filter(bus => {
+                return !!wheelchair ? (wheelchair === (bus.isWAB ? 'wab' : 'nwab')) : true
+            });
+
+                if (matchingTimings.length > 0) {
+                    busServiceTiming.timings = matchingTimings;
+                    possibleTimings[busStopCode] = busServiceTiming;
+                }
+            });
+        });
+
+        return possibleTimings;
+    }
+
+    static search(req, res) {
+        if (!req.body.query) {
+            res.status(400).json({
+                error: 'No query provided!'
+            });
+            return;
+        }
+
+        let timingsCache = BusTimings.getTimings();
+
+        let parsedData = BusSearcherRouter.parseData(req.body.query);
+        let possibleSvcs = BusSearcherRouter.getSvcsFromInput(parsedData);
+
+        let filteredServices = BusSearcherRouter.filterServices(timingsCache, possibleSvcs);
+
+        let nwabFiltered = BusSearcherRouter.filterByNWAB(filteredServices, parsedData.wheelchair);
+
+        console.log(JSON.stringify(nwabFiltered, null, 2));
+        console.log(parsedData);
     }
 
 }
