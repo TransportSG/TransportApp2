0a83e062d024a1391b12fb513f3744f53d248f9e
Eyeball Edward <edwardyeung.private@gmail.com>
2018-04-01 12:43:07 +0800

    barsemailer

---
+++ addons/BusEmailer/index.js
@@ -0,0 +1,95 @@
+const nodemailer = require('nodemailer');
+const Module = require('../../Module');
+const BusSearcherRouter = require('../../application/routes/BusSearcherRouter');
+const BusTimings = require('../BusTimings/BusTimings');
+
+const config = require('./bus-email-config.json');
+
+let previousData = {
+    svcsWithNWABs: [],
+    svcsWithBendies: []
+};
+
+class BusEmailer extends Module {
+
+    static getServiceList(timings) {
+        let busStopTimings = Object.values(timings);
+        let timingsList = busStopTimings.reduce((a, b) => a.concat(b), []);
+        let flattened = timingsList.map(timing => timing.service);
+
+        return flattened.filter((e, i, a) => a.indexOf(e) === i).sort();
+    }
+
+    static queryFunction() {
+        let timingsCache = BusTimings.getTimings();
+
+        let nwabBuses = BusSearcherRouter.filterByNWAB(timingsCache, 'nwab');
+        let bendyBuses = BusSearcherRouter.filterByType(timingsCache, 'BD');
+
+        let svcsWithNWABs = BusEmailer.getServiceList(nwabBuses);
+        let svcsWithBendies = BusEmailer.getServiceList(bendyBuses);
+
+        svcsWithNWABs = svcsWithNWABs.filter(svc => !svc.endsWith('M'));
+
+        return {svcsWithNWABs, svcsWithBendies};
+    }
+
+    static initEmail() {
+        BusEmailer.transporter = nodemailer.createTransport({
+         service: config.service,
+         auth: {
+                user: config.username,
+                pass: config.password
+            }
+        });
+    }
+
+    static sendMail(to, from, subject, html) {
+        BusEmailer.transporter.sendMail({
+            from, to, subject, html
+        }, function (err, info) {
+        });
+    }
+
+    static init() {
+        if (!config.active) {
+            return;
+        }
+
+        console.log('BusEmailer started');
+        BusEmailer.initEmail();
+
+        setInterval(() => {
+            let mailData = BusEmailer.queryFunction();
+
+            let shouldUpdate = (mailData.svcsWithNWABs.join('') !== previousData.svcsWithNWABs.join('')) ||
+                                (mailData.svcsWithBendies.join('') !== previousData.svcsWithBendies.join(''));
+
+            previousData = mailData;
+
+            if (shouldUpdate) {
+                let emailBody =
+`
+<h1>Bus update as of ${new Date().toString()}</h1>
+
+<p>Services with NWABS (Fake NWABs included): </p>
+<code>${mailData.svcsWithNWABs.join(', ')}</code>
+<br>
+<br>
+
+<p>Services with bendies (Wifi buses on 10 included): </p>
+<code>${mailData.svcsWithBendies.join(', ')}</code>
+`;
+
+                config.subscribers.forEach(email => {
+                    BusEmailer.sendMail(email, 'sbs9642p@gmail.com', 'Bus timing update', emailBody);
+
+                    console.log('BusEmailer: sent mail to', email)
+                });
+            }
+        }, 1000 * 60); // 1000ms = 1s; 1s * 60 = 1min;
+    }
+
+}
+
+module.exports = BusEmailer;
--- main.js
+++ main.js
@@ -1,11 +1,13 @@
 const DatabaseConnectionManager = require('./database/DatabaseConnectionManager');
 const MainServer = require('./server/MainServer');
 const HTTPRedirectServer = require('./server/HTTPRedirectServer');
+const BusEmailer = require('./addons/BusEmailer/index');
 
 const modules = [
     DatabaseConnectionManager,
     MainServer,
-    HTTPRedirectServer
+    HTTPRedirectServer,
+    BusEmailer
 ];
 
 modules.forEach(module => module.init());
