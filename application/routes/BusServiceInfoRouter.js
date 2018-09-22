ffcca4f7d4ad290864ceb8416a1a41faee2b387e
Eyeball Edward <edwardyeung.private@gmail.com>
2018-09-22 09:58:31 +0800

    show timings too

--- application/routes/BusServiceInfoRouter.js
+++ application/routes/BusServiceInfoRouter.js
@@ -2,6 +2,15 @@ const Router = require('./Router');
 const BusStopsRepository = require('../../database/BusStopsRepository');
 const BusServiceRepository = require('../../database/BusServiceRepository');
 const DatabaseConnectionManager = require('../../database/DatabaseConnectionManager');
+const BusTimings = require('../../addons/BusTimings/BusTimings');
+
+var timingDiff = (a, b) => {
+    var diff = new Date(Math.abs(a - b));
+    return {
+        minutes: diff.getUTCMinutes(),
+        seconds: diff.getUTCSeconds(),
+    }
+};
 
 class BusServiceInfoRouter extends Router {
 
@@ -26,8 +35,20 @@ class BusServiceInfoRouter extends Router {
                 data = data.map(a=>a[0]);
                 service.interchanges = data;
 
+                let finalTimings = {};
+                service.stops.forEach(busStop => {
+                    let bsc = busStop.busStopCode;
+                    let timings = BusTimings.getTimings()[bsc] || [];
+
+                    let serviceTimings = timings.filter(t => t.service === svc)[0] || {timings: []};
+
+                    finalTimings[bsc] = serviceTimings.timings.slice(0, 2);
+                });
+
                 res.render('bus-service-info/index', {
-                    service
+                    service,
+                    timings: finalTimings,
+                    timingDiff
                 });
             });
         });
