856d0df7bb1a8baec57488d6e29e3f2178cb4a7b
Eyeball Edward <edwardyeung.private@gmail.com>
2018-10-07 00:38:53 +0800

    logging 123

---
+++ addons/Log123/index.js
@@ -0,0 +1,32 @@
+const Module = require('../../Module');
+const BusTimings = require('../BusTimings/BusTimings');
+const fs = require('fs');
+
+class Log123 extends Module {
+
+    static find() {
+        let timingsCache = JSON.parse(JSON.stringify(BusTimings.getTimings()));
+
+        let bmiTimings = timingsCache[10009];
+        if (!bmiTimings) return;
+        let timings123 = bmiTimings.filter(timing => timing.service == '123');
+        if (!timings123) return;
+
+        timings123.forEach(bus => {
+            if (bus.type == 2) {
+                let curTime = new Date();
+                let depDiff = Math.abs(bus.arrivalTime - curTime);
+                if (depDiff > 60 * 1000) {
+                    fs.appendFile('123.txt', depTime + '\n', () => {});
+                }
+            }
+        });
+    }
+
+    static init() {
+        setTimeout(find, 1000);
+    }
+
+}
+
+module.exports = Log123;
--- main.js
+++ main.js
@@ -2,12 +2,14 @@ const DatabaseConnectionManager = require('./database/DatabaseConnectionManager'
 const MainServer = require('./server/MainServer');
 const HTTPRedirectServer = require('./server/HTTPRedirectServer');
 const BusEmailer = require('./addons/BusEmailer/index');
+const Log123 = require('./addons/Log123/index');
 
 const modules = [
     DatabaseConnectionManager,
     MainServer,
     HTTPRedirectServer,
-    BusEmailer
+    BusEmailer,
+    Log123
 ];
 
 modules.forEach(module => module.init());
